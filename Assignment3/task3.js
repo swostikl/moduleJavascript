bd6047d56c430398c1819d8b3fff7c3ad419675f
Swostika lama <swostikl@metropolia.fi>
2024-11-08 13:51:42 +0200

    second commit

---
+++ Assignment3/task3.js
@@ -0,0 +1,13 @@
+'use strict'
+const int1=prompt("Enter the 1st integer :");
+const int2=prompt("Enter a 2nd integer");
+const int3=prompt("Enter a 3rd integer");
+
+const a=parseInt(int1);
+const b=parseInt(int2);
+const c=parseInt(int3);
+const sum=a+b+c;
+const product=a*b*c;
+const average=(a+b+c) / 3;{
+    alert(`Sum: ${sum}, Product: ${product} , Average:${average}`);
+}
