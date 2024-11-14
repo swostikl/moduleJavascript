aac4dbd2190118d852d546172ef43f7030527146
Swostika lama <swostikl@metropolia.fi>
2024-11-14 12:59:37 +0200

    initial commit

---
+++ module2javascript/Assignment 7/task7.js
@@ -0,0 +1,21 @@
+function rollDice(sides) {
+    return Math.floor(Math.random() * sides) + 1;
+}
+function rollMax(sides) {
+    const rolls = [];
+    let roll;
+    do {
+        roll = rollDice(sides);
+        rolls.push(roll);
+        console.log(`Rolled: ${roll}`);
+    } while (roll !== sides);
+
+    console.log("Final rolls:", rolls);
+}
+const sides = parseInt(prompt("Enter the number of sides on the dice:"));
+
+if (sides > 0) {
+    rollMax(sides);
+} else {
+    alert("Please enter a valid positive number for the number of sides.");
+}
