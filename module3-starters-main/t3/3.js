e33bbd171daf09778d94d8045f6562363a02ba9c
Swostika Lama <swostikl@metropolia.fi>
2024-11-24 11:39:15 +0200

    Delete module3-starters-main directory

--- module3-starters-main/t3/3.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const element=document.getElementById("target")
-const names = ['John', 'Paul', 'Jones'];
-let htmlContent="";
-for(let i=0; i<names.length; i++){
-    htmlContent+=`<li>${names[i]}</li>`
-}
-element.innerHTML=htmlContent
-
--- module3-starters-main/t6/6.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const button=document.getElementById("alertButton")
-button.addEventListener("click",()=>{
-    alert("Button Clicked");
-})
-
-
